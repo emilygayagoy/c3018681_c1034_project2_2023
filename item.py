1d29fb41fea01ab6d5441e88dde1a5c10e27711d
emilygayagoy <c3018681@newcastle.ac.uk>
2023-10-30 13:59:16 +0000

    defining all functions in item class

--- item.py
+++ item.py
@@ -1,31 +1,44 @@
+import csv
+with open('sample_data.csv', mode = 'r') as file:
+    csv_file = csv.reader(file)
+    for lines in csv_file:
+        print(lines)
 class Item:
 
     def __init__(self, name, category, perishable, stock, sell_price):
-        pass
+        #initialise attributes
+        self.name = name
+        self.category = category
+        self.perishable = perishable
+        self.stock = stock
+        self.sell_price = sell_price
 
     def get_name(self):
-        pass
+        return self.name
 
     def get_category(self):
-        pass
+        return self.category
 
     def get_perishable(self):
-        pass
+        return self.perishable
 
     def get_stock(self):
-        pass
+        return self.stock
 
     def get_sell_price(self):
-        pass
+        return self.sell_price
 
     def __str__(self):
-        pass
+        return f'Item: {self.name}, {self.category}, {self.perishable}, {self.stock}, {self.sell_price}'
 
     def __repr__(self):
-        pass
+        return f'Item: {self.name}, {self.category}, {self.perishable}, {self.stock}, {self.sell_price}'
 
     def __eq__(self, other):
-        pass
+        if isinstance(other, Item): #checks if other is same data type as Item
+            if self.name == other.name: #checks if two items are equal to each other through the name
+                return True
+        return False
 
     def __hash__(self):
-        pass
+        return hash((self.name, self.category, self.perishable, self.stock, self.sell_price))
