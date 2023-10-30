d3bb924f5bfbf7b1914a55d590b7847f425e79b3
emilygayagoy <104984085+emilygayagoy@users.noreply.github.com>
2023-10-30 12:59:05 +0000

    uploading files from canvas to repository

---
+++ item.py
@@ -0,0 +1,31 @@
+class Item:
+
+    def __init__(self, name, category, perishable, stock, sell_price):
+        pass
+
+    def get_name(self):
+        pass
+
+    def get_category(self):
+        pass
+
+    def get_perishable(self):
+        pass
+
+    def get_stock(self):
+        pass
+
+    def get_sell_price(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __eq__(self, other):
+        pass
+
+    def __hash__(self):
+        pass
